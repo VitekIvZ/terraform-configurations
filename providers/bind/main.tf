463fb725c2671fbe646701cf3cceb15f98a21ea9
Vitek <vitek@my.domain>
2021-04-13 19:54:26 +0300

    Add modules

--- providers/bind/main.tf
+++ providers/bind/main.tf
@@ -1,28 +1,28 @@
-locals {
-  minions = ["bob", "kevin", "stewart"]
-}
-resource "null_resource" "minions" {
-  for_each = toset(local.minions)
-  triggers = {
-    name = each.value
-  }
-}
-output "minions" {
-  value = null_resource.minions
-}
+// locals {
+//  minions = ["bob", "kevin", "stewart"]
+//}
+//resource "null_resource" "minions" {
+//  for_each = toset(local.minions)
+//  triggers = {
+//    name = each.value
+ // }
+//}
+//output "minions" {
+//  value = null_resource.minions
+//}
 
-locals {
-  names = ["bob", "kevin", "stewart", "devops"]
-}
-resource "null_resource" "names" {
-  count = length(local.names)
-  triggers = {
-    name = local.names[count.index]
-  }
-}
-output "names" {
-  value = null_resource.names
-}
+//locals {
+//  names = ["bob", "kevin", "stewart", "devops"]
+//}
+//resource "null_resource" "names" {
+//  count = length(local.names)
+//  triggers = {
+//    name = local.names[count.index]
+//  }
+//}
+//output "names" {
+//  value = null_resource.names
+//}
 
 resource "dns_a_record_set" "test_1803" {
     zone = "my.vizlab.com."
@@ -33,12 +33,20 @@ resource "dns_a_record_set" "test_1803" {
     ttl = 300
 }
 
-resource "dns_a_record_set" "test2_1803" {
-  zone = local.domain
-  name = var.a_record
-  addresses = var.ip
-  ttl = 300
-}
+//resource "dns_txt_record_set" "google" {
+//  zone = "example.com."
+//  txt = [
+//    "google-site-verification=...",
+//  ]
+//  ttl = 300
+//}
+
+//resource "dns_a_record_set" "test2_1803" {
+//  zone = local.domain
+//  name = var.a_record
+//  addresses = var.ip
+//  ttl = 300
+//}
 
 //resource "null_resource" "simple" {
 //  count = 2
