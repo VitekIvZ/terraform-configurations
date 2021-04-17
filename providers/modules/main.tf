d5c12945e9d4dfc918cafa0d237596294d052785
Vitek <vitek@my.domain>
2021-04-17 20:59:15 +0300

    Add new edition module

--- providers/modules/main.tf
+++ providers/modules/main.tf
@@ -1,3 +1,16 @@
+provider "dns" {
+  version = "~> 3.0"
+  update {
+    server        = "192.168.206.129"
+    key_name      = "terr-key."
+    key_algorithm = "hmac-md5"
+    key_secret    = "gJrbGvHtyob4zwAdzkSDPw=="
+    //key_algorithm = "hmac-sha256"
+    //key_secret = "y3G6kvYy25JQYUxKdvuae30xD5uG0RooivUPDUh2MLc="
+
+  }
+}
+
 resource "dns_a_record_set" "this" {
     zone = var.zone
     name = var.a_record
@@ -10,3 +23,4 @@ resource "dns_txt_record_set" "this" {
     txt = var.txt_name
     ttl = var.ttl
 }
+
